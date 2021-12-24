181c110045e135b23986711a26b39417ec22e70a
Mady-Camara <mysteremady@gmail.com>
2021-12-24 17:32:12 +0000

    header, footer and section integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
